3d172c9ffe6cbd8e9160827eed5efeb5631a935c
oowuyue <oowuyue@gmail.com>
2024-11-06 14:03:09 +0800

    标星

--- cn/雪球行情/指数策略.js
+++ cn/雪球行情/指数策略.js
@@ -3994,3 +3994,9 @@ var triggerLogObj指数 = {
   ]
 }
 var runLastKlineDate = "2024-11-05"
+
+
+if (typeof module !== "undefined" && module.exports) {
+  exports.triggerLogObj指数 = triggerLogObj指数
+  exports.runLastKlineDate = runLastKlineDate
+}
