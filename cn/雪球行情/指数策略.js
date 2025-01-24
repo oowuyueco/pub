35d3b995ff6360fd4623c90780b8de0fd83f084c
oowuyueco <oowuyue@163.com>
2025-01-24 17:34:30 +0800

    gitAct_quant_1-5

--- cn/雪球行情/指数策略.js
+++ cn/雪球行情/指数策略.js
@@ -5711,7 +5711,7 @@ var triggerLogObj指数 = {
     }
   ]
 }
-var runLastKlineDate = "2025-01-23"
+var runLastKlineDate = "2025-01-24"
 if (typeof module !== "undefined" && module.exports) {
     exports.triggerLogObj指数 = triggerLogObj指数
     exports.runLastKlineDate = runLastKlineDate
