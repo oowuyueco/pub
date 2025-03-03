220947c4151e11acc6b8a7073154659035fe7ecb
oowuyue <oowuyue@163.com>
2025-03-03 16:36:32 +0800

    premScr

--- js/my.js
+++ js/my.js
@@ -213,17 +213,19 @@ function isSameWeek(d1, d2) {
     return !(difftime >= ONE_DAY * 7 || bigDay < smallDay || (bigDay === smallDay && difftime > ONE_DAY))
 }
 
-function isSameMonth(date) {
-    var current_date = new Date();
-    var current_y = current_date.getFullYear(),
-        current_m = current_date.getMonth() + 1,
-        current_d = current_date.getDate();
-    if (current_m < 10)
-        current_m = '0' + current_m;
-    if (current_d < 10)
-        current_d = '0' + current_d;
+function isSameMonth(date1, date2) {
+    if (!date2) date2 = new Date()
+    else date2 = new Date(date2)
 
-    if (date.substring(0, 7) == (current_y + '-' + current_m)) { //当月不处理
+    var date2_y = date2.getFullYear(),
+        date2_m = date2.getMonth() + 1,
+        date2_d = date2.getDate();
+    if (date2_m < 10)
+        date2_m = '0' + date2_m;
+    if (date2_d < 10)
+        date2_d = '0' + date2_d;
+
+    if (date1.substring(0, 7) == (date2_y + '-' + date2_m)) { //当月不处理
         return true
     } else {
         return false
@@ -378,8 +380,10 @@ Array.prototype.akData2Obj = function (period = "day") {
                 "low": data.最低,
                 "close": data.收盘,
                 "volume": data.成交量,
+                "hsl": data?.换手率,
+                "zf": data?.振幅,
+                "percent": data?.涨跌幅 ?? PtPPercent(dataList[index - 1], data),
                 "timestamp": "",
-                "percent": PtPPercent(dataList[index - 1], data)
             }
         }
 
@@ -515,6 +519,208 @@ Array.prototype.ohlc2oclh = function () {
 
     return dataList
 }
+
+
+//--------------------------quotechart2022/src/modules/tools/indicator/cyq.ts----------
+//--------------------------webpack://quotechart2022/src/modules/kline/calculate.ts----
+//--------------------------webpack://quotechart2022/src/modules/kline/cyq.ts----------
+function myCYQCalculator(klinedata, index) {
+    var maxprice = 0;
+    var minprice = 0;
+
+    var factor = 150;
+    var start = 0;
+
+    /**
+     * K图数据[time,open,close,high,low,volume,amount,amplitude,turnoverRate]
+     */
+    var kdata = klinedata.slice(start, Math.max(1, index + 1));
+    if (kdata.length === 0) throw "invaild index";
+    for (var i = 0; i < kdata.length; i++) {
+        var elements = kdata[i];
+        maxprice = !maxprice ? elements.high : Math.max(maxprice, elements.high);
+        minprice = !minprice ? elements.low : Math.min(minprice, elements.low);
+    }
+
+    // 精度不小于0.01 产品逻辑
+    var accuracy = Math.max(0.01, (maxprice - minprice) / (factor - 1));
+    /**
+     * 值域
+     * @type {Array.<number>}
+     */
+    var yrange = [];
+    for (var i = 0; i < factor; i++) {
+        yrange.push((minprice + accuracy * i).toFixed(2) / 1);
+    }
+    /**
+     * 横轴数据
+     */
+    var xdata = createNumberArray(factor);
+
+    for (var i = 0; i < kdata.length; i++) {
+        var eles = kdata[i];
+        var open = eles.open,
+            close = eles.close,
+            high = eles.high,
+            low = eles.low,
+            avg = (open + close + high + low) / 4,
+            turnoverRate = Math.min(1, eles.hsl / 100 || 0);
+        var H = Math.floor((high - minprice) / accuracy),
+            L = Math.ceil((low - minprice) / accuracy),
+            // G点坐标, 一字板时, X为进度因子
+            GPoint = [
+                high == low ? factor - 1 : 2 / (high - low),
+                Math.floor((avg - minprice) / accuracy),
+            ];
+        // 衰减
+        for (var n = 0; n < xdata.length; n++) {
+            xdata[n] *= 1 - turnoverRate;
+        }
+        if (high == low) {
+            // 一字板时，画矩形面积是三角形的2倍
+            xdata[GPoint[1]] += (GPoint[0] * turnoverRate) / 2;
+        } else {
+            for (var j = L; j <= H; j++) {
+                var curprice = minprice + accuracy * j;
+                if (curprice <= avg) {
+                    // 上半三角叠加分布分布
+                    if (Math.abs(avg - low) < 1e-8) {
+                        xdata[j] += GPoint[0] * turnoverRate;
+                    } else {
+                        xdata[j] +=
+                            ((curprice - low) / (avg - low)) * GPoint[0] * turnoverRate;
+                    }
+                } else {
+                    // 下半三角叠加分布分布
+                    if (Math.abs(high - avg) < 1e-8) {
+                        xdata[j] += GPoint[0] * turnoverRate;
+                    } else {
+                        xdata[j] +=
+                            ((high - curprice) / (high - avg)) * GPoint[0] * turnoverRate;
+                    }
+                }
+            }
+        }
+    }
+
+    var currentprice = klinedata[index].close;
+    var totalChips = 0;
+    for (var i = 0; i < factor; i++) {
+        var x = xdata[i].toPrecision(12) / 1;
+        //if (x < 0) xdata[i] = 0;
+        totalChips += x;
+    }
+    var result = new CYQData();
+    result.x = xdata;
+    result.y = yrange;
+    result.benefitPart = result.getBenefitPart(currentprice);
+    result.avgCost = getCostByChip(totalChips * 0.5).toFixed(2);
+    result.percentChips = {
+        90: result.computePercentChips(0.9),
+        70: result.computePercentChips(0.7),
+    };
+    return result;
+
+    /**
+     * 获取指定筹码处的成本
+     * @param {number} chip 堆叠筹码
+     */
+    function getCostByChip(chip) {
+        var result = 0,
+            sum = 0;
+        for (var i = 0; i < factor; i++) {
+            var x = xdata[i].toPrecision(12) / 1;
+            if (sum + x > chip) {
+                result = minprice + i * accuracy;
+                break;
+            }
+            sum += x;
+        }
+        return result;
+    }
+
+    /**
+     * 筹码分布数据
+     */
+    function CYQData() {
+        /**
+         * 筹码堆叠
+         * @type {Array.<number>}
+         */
+        this.x = arguments[0];
+        /**
+         * 价格分布
+         * @type {Array.<number>}
+         */
+        this.y = arguments[1];
+        /**
+         * 获利比例
+         * @type {number}
+         */
+        this.benefitPart = arguments[2];
+        /**
+         * 平均成本
+         * @type {number}
+         */
+        this.avgCost = arguments[3];
+        /**
+         * 百分比筹码
+         * @type {{Object.<string, {{priceRange: number[], concentration: number}}>}}
+         */
+        this.percentChips = arguments[4];
+        /**
+         * 计算指定百分比的筹码
+         * @param {number} percent 百分比大于0，小于1
+         */
+        this.computePercentChips = function (percent) {
+            if (percent > 1 || percent < 0) throw 'argument "percent" out of range';
+            var ps = [(1 - percent) / 2, (1 + percent) / 2];
+            var pr = [
+                getCostByChip(totalChips * ps[0]),
+                getCostByChip(totalChips * ps[1]),
+            ];
+            return {
+                priceRange: [pr[0].toFixed(2), pr[1].toFixed(2)],
+                concentration:
+                    pr[0] + pr[1] === 0 ? 0 : (pr[1] - pr[0]) / (pr[0] + pr[1]),
+            };
+        };
+        /**
+         * 获取指定价格的获利比例
+         * @param {number} price 价格
+         */
+        this.getBenefitPart = function (price) {
+            var below = 0;
+            for (var i = 0; i < factor; i++) {
+                var x = xdata[i].toPrecision(12) / 1;
+                if (price >= minprice + i * accuracy) {
+                    below += x;
+                }
+            }
+            return totalChips == 0 ? 0 : below / totalChips;
+        };
+    }
+}
+function createNumberArray(count) {
+    var array = [];
+    for (var i = 0; i < count; i++) {
+        array.push(0);
+    }
+    return array;
+}
+Array.prototype.mapCYQCalculator = function () {
+    let dataList = this;
+    dataList = dataList.map((data, index) => {
+        let CYQ = myCYQCalculator(dataList, index);
+        delete CYQ.x;
+        delete CYQ.y;
+        data.CYQ = CYQ
+        // data.benfPart = +CYQ.benefitPart.toFixed(4)
+        return data;
+    });
+    return dataList;
+};
+
 
 //技术指标https://github.com/kimboqi/stock-indicators
 function dayToPeriod(dayIndexList, period) {
@@ -1128,5 +1334,7 @@ if (typeof module !== "undefined" && module.exports) {
     exports.getRandom登陆名 = getRandom登陆名
 
     exports.myjscurrentDayYMD = currentDayYMD
+    exports.myCYQCalculator = myCYQCalculator
+    
 
 } 
