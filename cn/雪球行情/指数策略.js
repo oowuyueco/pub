af2fd511fe983d9c541c9555e2def373c8573614
oowuyueco <oowuyue@163.com>
2025-09-23 17:38:56 +0800

    gitAct_quant_1-5

--- cn/雪球行情/指数策略.js
+++ cn/雪球行情/指数策略.js
@@ -4298,7 +4298,7 @@ var triggerLogObj指数 = {
           "low": "2->2025-08-27,-83.09,-1.86"
         },
         "after": {
-          "close": "20->2025-09-22,53.39,1.19",
+          "close": "21->2025-09-23,50.56,1.13",
           "high": "17->2025-09-17,81.8,1.83",
           "low": "8->2025-09-04,-104.01,-2.33",
           "nextSecondDelivery周三": "2025-09-24",
@@ -4316,7 +4316,7 @@ var triggerLogObj指数 = {
           "low": "1->2025-08-27,-66.46,-1.49"
         },
         "after": {
-          "close": "19->2025-09-22,70.02,1.57",
+          "close": "20->2025-09-23,67.19,1.51",
           "high": "16->2025-09-17,98.43,2.21",
           "low": "7->2025-09-04,-87.38,-1.96",
           "nextSecondDelivery周三": "2025-09-24",
@@ -6450,7 +6450,7 @@ var triggerLogObj指数 = {
     }
   ]
 }
-var runLastKlineDate = "2025-09-22"
+var runLastKlineDate = "2025-09-23"
 if (typeof module !== "undefined" && module.exports) {
     exports.triggerLogObj指数 = triggerLogObj指数
     exports.runLastKlineDate = runLastKlineDate
