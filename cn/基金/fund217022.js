c320269bfdc07dd361956bd5c7c307f06a01681a
oowuyueco <oowuyue@163.com>
2025-11-19 03:23:09 +0800

    gitAct_ths_next01_ths1

--- cn/基金/fund217022.js
+++ cn/基金/fund217022.js
@@ -7196,7 +7196,7 @@ var 招商债券_沪深300_创业板_三年定期 = [
         "三年定期": 52.828399999999995
     }
 ];
-var runDate = "2025-11-18";
+var runDate = "2025-11-19";
 if (typeof module !== "undefined" && module.exports) {
     exports.招商债券_沪深300_创业板_三年定期 = 招商债券_沪深300_创业板_三年定期
 }
--- cn/基金/基金策略.js
+++ cn/基金/基金策略.js
@@ -820,4 +820,4 @@ var triggerLogObj基金 = {
         }
     ]
 };
-var runDateTime基金策略 = "2025-11-18 20:18:02";
+var runDateTime基金策略 = "2025-11-19 03:21:32";
