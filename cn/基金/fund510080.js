416ba77563457002242c57b3450430230cf781c8
oowuyueco <oowuyue@163.com>
2025-11-22 03:23:28 +0800

    gitAct_ths_next01_ths1

--- cn/基金/fund510080.js
+++ cn/基金/fund510080.js
@@ -11557,7 +11557,7 @@ var 长盛全债_沪深300_上证_债A股8 = [
         "债A股8": null
     }
 ];
-var runDate = "2025-11-21";
+var runDate = "2025-11-22";
 if (typeof module !== "undefined" && module.exports) {
     exports.长盛全债_沪深300_上证_债A股8 = 长盛全债_沪深300_上证_债A股8
 }
