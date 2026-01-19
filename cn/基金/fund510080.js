f5607e59b17bf1e2a9313a7a8d21c18aa4a792f6
oowuyueco <oowuyue@163.com>
2026-01-20 03:26:14 +0800

    gitAct_ths_next01_ths1

--- cn/基金/fund510080.js
+++ cn/基金/fund510080.js
@@ -11641,7 +11641,7 @@ var 长盛全债_沪深300_上证_债A股8 = [
         "债A股8": null
     }
 ];
-var runDate = "2026-01-19";
+var runDate = "2026-01-20";
 if (typeof module !== "undefined" && module.exports) {
     exports.长盛全债_沪深300_上证_债A股8 = 长盛全债_沪深300_上证_债A股8
 }
--- cn/基金/基金策略.js
+++ cn/基金/基金策略.js
@@ -545,4 +545,4 @@ var triggerLogObj基金 = {
         }
     ]
 };
-var runDateTime基金策略 = "2026-01-19 20:21:00";
+var runDateTime基金策略 = "2026-01-20 03:24:28";
