c0ae81723186dd0e366139bb64453ec30953a1f8
oowuyueco <oowuyue@163.com>
2026-02-26 03:52:57 +0800

    gitAct_ths_next01_ths1

--- cn/基金/fund002146.js
+++ cn/基金/fund002146.js
@@ -4491,10 +4491,10 @@ var 长安鑫益_沪深300_债B股15 = [
         "date": "2026-02-25",
         "长安鑫益": 51.73,
         "沪深300": 60.6459,
-        "债B股15": null
+        "债B股15": 18.3613
     }
 ];
-var runDate = "2026-02-25";
+var runDate = "2026-02-26";
 if (typeof module !== "undefined" && module.exports) {
     exports.长安鑫益_沪深300_债B股15 = 长安鑫益_沪深300_债B股15
 }
--- cn/基金/基金策略.js
+++ cn/基金/基金策略.js
@@ -545,4 +545,4 @@ var triggerLogObj基金 = {
         }
     ]
 };
-var runDateTime基金策略 = "2026-02-25 20:33:28";
+var runDateTime基金策略 = "2026-02-26 03:51:16";
