4d61f743459d9eb614aff9a9be810c8caea0f164
oowuyueco <oowuyue@163.com>
2026-03-14 02:54:59 +0800

    gitAct_3pmiFundThs_pmiFund

--- cn/基金/fund002146.js
+++ cn/基金/fund002146.js
@@ -4509,10 +4509,10 @@ var 长安鑫益_沪深300_债B股15 = [
         "date": "2026-03-13",
         "长安鑫益": 51.77,
         "沪深300": 58.3817,
-        "债B股15": null
+        "债B股15": 17.9842
     }
 ];
-var runDate = "2026-03-13";
+var runDate = "2026-03-14";
 if (typeof module !== "undefined" && module.exports) {
     exports.长安鑫益_沪深300_债B股15 = 长安鑫益_沪深300_债B股15
 }
