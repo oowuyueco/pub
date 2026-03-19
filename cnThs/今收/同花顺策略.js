eefcf940c51994f781d2d741b5bc50c29b390121
oowuyueco <oowuyue@163.com>
2026-03-19 20:37:13 +0800

    gitAct_3pmiFundThs_thsStrat

--- cnThs/今收/同花顺策略.js
+++ cnThs/今收/同花顺策略.js
@@ -309,5 +309,5 @@ var triggerLogObj同花顺 = {
 
     if (typeof module !== "undefined" && module.exports) {
         exports.triggerLogObj同花顺 = triggerLogObj同花顺
-        exports.同花顺策略runLastTime = "2026-03-18 20:45:54"
+        exports.同花顺策略runLastTime = "2026-03-19 20:37:12"
     }
