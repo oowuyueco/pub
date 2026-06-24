e90ecc8ac8a2d28ea58fcad2fdd382fabcf87d94
oowuyueco <oowuyue@163.com>
2026-06-24 20:12:45 +0800

    gitAct_fund_pmi_ths_1-6

--- cn/pmi股债策略.js
+++ cn/pmi股债策略.js
@@ -1303,4 +1303,4 @@ var triggerLogObjPmi股债 = {
     }
   ]
 }
-var pmi股债策略runLastTime = "2026-06-23 20:22:25"
+var pmi股债策略runLastTime = "2026-06-24 20:08:33"
--- cn/基金/基金策略.js
+++ cn/基金/基金策略.js
@@ -766,4 +766,4 @@ var triggerLogObj基金 = {
         }
     ]
 };
-var 基金策略runLastTime = "2026-06-23 20:22:21";
+var 基金策略runLastTime = "2026-06-24 20:08:29";
